d08d6ec1fd062d7f3d73a83d9d4ac7de6f4e1f4a
Zakin <alexandre.journet@gmail.com>
2018-03-13 11:18:56 +0100

    ADD GIF

--- Mumpus/src/view/PanneauPlateau.java
+++ Mumpus/src/view/PanneauPlateau.java
@@ -2,8 +2,14 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import java.util.Scanner;
 import javax.swing.JPanel;
@@ -37,10 +43,19 @@ public class PanneauPlateau extends JPanel {
 				ci = Skin(i, j);
 				this.buttons[i][j].setIcon(ci);
 				this.add(this.buttons[i][j]);
+				this.buttons[i][j].addMouseListener(new MouseAdapter() {
+					public void mousePressed(MouseEvent e) {
+						JButton a = (JButton) e.getSource();
+						//System.out.println(a.getX()/a.getWidth());
+						deplacerAgent(a.getX()/a.getWidth(), a.getY()/a.getHeight());
+					}
+				});
 			}
 		}
 		this.setVisible(true);
-		this.deplacerAgent(0, 0);
+		this.deplacerAgent(0, this.p.getSize() - 1);
+
+		System.out.println("Nope");
 	}
 
 	public PanneauPlateau() {
@@ -49,11 +64,21 @@ public class PanneauPlateau extends JPanel {
 	public void deplacerAgent(int newPosX, int newPosY){
 		int oldPosX=this.p.getAgent().getPosX();
 		int oldPosY = this.p.getAgent().getPosY();
-		this.p.deplacerAgent(newPosX, newPosY);
+		this.p.deplacerAgent(newPosY, newPosX);
+
+		CombinedIcon cio = (CombinedIcon) this.buttons[oldPosX][oldPosY].getIcon();
+
+		CombinedIcon ci = (CombinedIcon) this.buttons[newPosY][newPosX].getIcon();
+		ci.add(cio.getLastImageIcon(), this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+
+		cio.removeLast();
+
 		
-		this.Skin(oldPosX, oldPosY);
-		this.Skin(newPosX, newPosY);
+		this.setSkinAfterMovement(ci, newPosY, newPosX);
+		this.setSkinAfterMovement(cio, oldPosX, oldPosY);
 		this.refresh();
+		
+		popup(newPosY, newPosX);
 	}
 	public void setPanneauPlateau(Plateau p) {
 		this.p = p;
@@ -93,45 +118,142 @@ public class PanneauPlateau extends JPanel {
 	// Assigne un Skin aux diffï¿½rentes cases
 	public CombinedIcon Skin(int i, int j) {
 		ImageIcon c = new ImageIcon("Case.png");
+		if(i==0)
+			c = new ImageIcon("CaseBordure1.png");
+		if(i==this.buttons.length-1)
+			c = new ImageIcon("CaseBordure2.png");
+		if(j==0)
+			c = new ImageIcon("CaseBordure3.png");
+		if(j==this.buttons.length-1)
+			c = new ImageIcon("CaseBordure4.png");
+		if(i==0&&j==0)
+			c = new ImageIcon("CaseBordure6.png");
+		if(i==0&&j==buttons.length-1)
+			c = new ImageIcon("CaseBordure5.png");		
+		if(i==buttons.length-1&&j==0)
+			c = new ImageIcon("CaseBordure7.png");	
+		if(i==buttons.length-1&&j==buttons.length-1)
+			c = new ImageIcon("CaseBordure8.png");
+
 		CombinedIcon ci = new CombinedIcon();
-		ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+		ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 
 		if (p.getCase(i, j).isMurHaut()) {
 			c = new ImageIcon("Mur_h.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 
 		if (p.getCase(i, j).isMurDroit()) {
 			c = new ImageIcon("Mur_d.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 		if (p.getCase(i, j).isMurBas()) {
 			c = new ImageIcon("Mur_b.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 		if (p.getCase(i, j).isMurGauche()) {
 			c = new ImageIcon("Mur_g.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 		if(p.getCase(i, j).isWumpus()){
 			c = new ImageIcon("wumpus.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 		if(p.getCase(i, j).isPuit()){
 			c = new ImageIcon("puit.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 		if(p.getCase(i, j).isTresor()){
 			c = new ImageIcon("tresor.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
 		if(i==this.p.getSize()-1&&j==0){
 			c = new ImageIcon("Pion_rouge.png");
-			ci.add(c, this.getWidth() / this.buttons.length - 3, this.getHeight() / this.buttons.length - 3);
+			ci.add(c, plat.getWidth() / this.buttons.length - 3, plat.getHeight() / this.buttons.length - 3);
 		}
-			
-	return ci;
 
+		return ci;
+
+	}
+	
+	public void popup(int newPosY, int newPosX ){
+		if(p.getCase(newPosY,newPosX).isPuit())
+		{
+			JOptionPane jop = new JOptionPane();
+		
+			ImageIcon icon = new ImageIcon("putindance.gif");
+			int option = jop.showConfirmDialog(null, "Voulez vous recommencez ?","Perdu !", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, icon);
+
+			
+			if(option == JOptionPane.OK_OPTION){
+				
+				setVisible(false); 
+			
+				plat.lancerPartie(p.getSize(),plat.getHeight(), plat.getWidth());
+
+			}
+			if(option == JOptionPane.NO_OPTION){
+				
+				setVisible(false); 
+				plat.dispose();
+				
+				
+				Fenetre fen  = new Fenetre();
+						
+			}
+		}
+		
+		if(p.getCase(newPosY,newPosX).isWumpus())
+		{
+			JOptionPane jop = new JOptionPane();
+		
+			ImageIcon icon = new ImageIcon("paresseux2.gif");
+			int option = jop.showConfirmDialog(null, "Voulez vous recommencez ?","Perdu !", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, icon);
+
+			
+			if(option == JOptionPane.OK_OPTION){
+				
+				setVisible(false); 
+			
+				plat.lancerPartie(p.getSize(),plat.getHeight(), plat.getWidth());
+
+			}
+			if(option == JOptionPane.NO_OPTION){
+				
+				setVisible(false); 
+				plat.dispose();
+				
+				
+				Fenetre fen  = new Fenetre();
+						
+			}
+		}
+		
+		if(p.getCase(newPosY,newPosX).isTresor())
+		{
+			JOptionPane jop = new JOptionPane();
+		
+			ImageIcon icon = new ImageIcon("win.gif");
+			int option = jop.showConfirmDialog(null, "Voulez vous recommencez ?","Gagné !", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,icon);
+
+			
+			if(option == JOptionPane.OK_OPTION){
+				
+				setVisible(false); 
+			
+				plat.lancerPartie(p.getSize(),plat.getHeight(), plat.getWidth());
+
+			}
+			if(option == JOptionPane.NO_OPTION){
+				
+				setVisible(false); 
+				plat.dispose();
+				
+				
+				Fenetre fen  = new Fenetre();
+						
+			}
+		}
 	}
 	// Accesseurs
 	// ____________________________________________________________________
